3acd41bae5459bd92343bfc093673de7ca159e3e
xiaoyong <2927479284@qq.com>
2023-06-26 15:40:30 +0800

    前端体系课 vue day56完成+1

---
+++ js-体系课程/Vue3+项目实战/day56-Vue3全家桶实战_03/02_Learn_Component/02_product_demo/src/components/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h2>{{title}}</h2>
+  <h2>当前计数: {{counter}}</h2>
+  <button @click="increment">+1</button>
+  <button @click="decrement">-1</button>
+
+  <product-item></product-item>
+</template>
+
+<script>
+  import productItem from "@/components/ProductItem.vue";
+  export default {
+    data(){
+      return{
+        title: "我是标题",
+        counter: 1,
+      }
+    },
+    methods:{
+      increment(){
+        this.counter++;
+      },
+      decrement(){
+        this.counter--;
+      }
+    },
+    components:{
+      "product-item": productItem
+    }
+  }
+</script>
+
+<style>
+  h2 {
+    color: red;
+  }
+</style>
