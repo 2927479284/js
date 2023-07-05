31dc816c8957cbb41b3dfe74615194e7f25f86ca
xiaoyong <2927479284@qq.com>
2023-07-05 18:27:41 +0800

    day 60结束

---
+++ js-体系课程/Vue3+项目实战/day60-Vue3全家桶实战_07/04_learn_composition/src/09_Setup-侦听数据变化-自写/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>{{ info1.name }}</h2>
+  </div>
+  <button @click="onClick">修改数据</button>
+</template>
+
+<script>
+  import { watchEffect, watch, ref } from 'vue'
+
+  export default {
+    setup() {
+      let info1 = ref({
+        name: "张三",
+        age: "188"
+      });
+      watch(info1,(newValue,oldValue)=>{
+        console.log(newValue,oldValue);
+      },{deep: true});
+
+      function onClick(){
+        info1.value.name = "李四"
+        console.log(info1)
+      }
+
+      return {
+        info1: info1,
+        onClick
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
+
--- js-体系课程/Vue3+项目实战/day60-Vue3全家桶实战_07/04_learn_composition/src/main.js
+++ js-体系课程/Vue3+项目实战/day60-Vue3全家桶实战_07/04_learn_composition/src/main.js
@@ -8,7 +8,8 @@ import { createApp } from 'vue'
 // import App from './07_Setup生命周期函数/App.vue'
 // import App from './08_Setup-Provide-Inject/App.vue'
 // import App from './09_Setup-侦听数据变化/App.vue'
-import App from './10_Setup-Hooks练习/App.vue'
+import App from './09_Setup-侦听数据变化-自写/App.vue'
+// import App from './10_Setup-Hooks练习/App.vue'
 // import App from './11_script_setup语法/App.vue'
 
 createApp(App).mount('#app')
