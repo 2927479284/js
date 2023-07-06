92ce49ea4abaee10a1bb37bdbc35d62172632034
xiaoyong <2927479284@qq.com>
2023-07-06 11:53:41 +0800

    day61 基础代码结构

---
+++ js-体系课程/Vue3+项目实战/day61-Vue3全家桶实战_08/07_learn_vuex/src/store/index.js
@@ -0,0 +1,62 @@
+import { createStore } from 'vuex'
+import { CHANGE_INFO } from './mutation_types'
+
+const store = createStore({
+  state: () => ({
+    counter: 100,
+    name: "coderwhy",
+    level: 100,
+    avatarURL: "http://xxxxxx",
+    friends: [
+      { id: 111, name: "why", age: 20 },
+      { id: 112, name: "kobe", age: 30 },
+      { id: 113, name: "james", age: 25 }
+    ]
+  }),
+  getters: {
+    // 1.基本使用
+    doubleCounter(state) {
+      return state.counter * 2
+    },
+    totalAge(state) {
+      return state.friends.reduce((preValue, item) => {
+        return preValue + item.age
+      }, 0)
+    },
+    // 2.在该getters属性中, 获取其他的getters
+    message(state, getters) {
+      return `name:${state.name} level:${state.level} friendTotalAge:${getters.totalAge}`
+    },
+    // 3.getters是可以返回一个函数的, 调用这个函数可以传入参数(了解)
+    getFriendById(state) {
+      return function(id) {
+        const friend = state.friends.find(item => item.id === id)
+        return friend
+      }
+    }
+  },
+  mutations: {
+    increment(state) {
+      state.counter++
+    },
+    changeName(state, payload) {
+      state.name = payload
+    },
+    incrementLevel(state) {
+      state.level++
+    },
+    [CHANGE_INFO](state, newInfo) {
+      state.level = newInfo.level
+      state.name = newInfo.name
+
+      // 重要的原则: 不要在mutation方法中执行异步操作
+      // fetch("xxxx").then(res => {
+      //   res.json().then(res => {
+      //     state.name = res.name
+      //   })
+      // })
+    }
+  }
+})
+
+export default store
