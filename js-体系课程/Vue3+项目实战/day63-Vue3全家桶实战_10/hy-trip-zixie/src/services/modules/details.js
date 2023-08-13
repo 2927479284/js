07c7820f8c7f70a6448f71c97eed25e823662870
xiaoyong <2927479284@qq.com>
2023-08-13 12:19:04 +0800

    day66完成

---
+++ js-体系课程/Vue3+项目实战/day63-Vue3全家桶实战_10/hy-trip-zixie/src/services/modules/details.js
@@ -0,0 +1,11 @@
+import hyRequest from "../request";
+
+
+export function getDetailInfos(houseId){
+    return hyRequest.get({
+        url: "/detail/infos",
+        params: {
+            "houseId": houseId
+        }
+    })
+}
--- js-体系课程/Vue3+项目实战/day63-Vue3全家桶实战_10/hy-trip-zixie/vite.config.js
+++ js-体系课程/Vue3+项目实战/day63-Vue3全家桶实战_10/hy-trip-zixie/vite.config.js
@@ -5,21 +5,17 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Components({
       resolvers: [VantResolver()],
-    }),
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server:{
-    port: 81
   }
 })
